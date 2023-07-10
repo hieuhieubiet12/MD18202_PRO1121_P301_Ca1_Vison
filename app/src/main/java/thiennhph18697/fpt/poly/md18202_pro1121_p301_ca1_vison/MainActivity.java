c5df7c44bc773d1242894178715ca1496aa7fd7f
thien <linkstart1410@gmail.com>
2023-07-10 08:46:15 +0700

    Initial commit

---
+++ app/src/main/java/thiennhph18697/fpt/poly/md18202_pro1121_p301_ca1_vison/MainActivity.java
@@ -0,0 +1,14 @@
+package thiennhph18697.fpt.poly.md18202_pro1121_p301_ca1_vison;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/thiennhph18697/fpt/poly/md18202_pro1121_p301_ca1_vison/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package thiennhph18697.fpt.poly.md18202_pro1121_p301_ca1_vison;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
